c9c4a74f18ebde59eefdbfa6f31d8128b321afb8
furquan <ubedmirza90@gmail.com>
2023-12-18 16:08:04 +0530

    Day-4 of python

---
+++ Day4/new_function.py
@@ -0,0 +1,17 @@
+def add(num1, num2):
+    add=num1+num2
+    return add
+def sub(num1, num2):
+    sub=num1-num2
+    return sub
+def mul(num1, num2):
+    mul=num1*num2
+    return mul
+def div(num1, num2):
+    div=num1/num2
+    return div
+
+print("Addition is:",add(2,3))
+print("Substraction is",sub(5,2))
+print("Multiplication is",mul(3,4))
+print("Division is:",div(6,2))
